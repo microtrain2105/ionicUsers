f04a52bf73ebdf5b6a6282f9ee6caa3adecffe37
chad svastisalee <swasgrafx@gmail.com>
2021-07-14 16:44:22 -0500

    Implement User Model Import AuthService Login Page

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+// 1. Import HttpHeaders, this will allow us to POST JSON data
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+// 2. Import Observable 
+import { Observable } from 'rxjs';
+
+// 3. Import User
+import { User } from './user.model';
+
+// 2. Create a JSON header
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+    // 3. Add URL as an instance variable
+    private url: string;
+
+    constructor(private http: HttpClient) {
+      let l = window.location;
+      let host:string;
+      //Are we running under Ionic or in a production environment?
+      if(l.port >= '8100'){
+        host = 'localhost:3000';
+      }else{
+        host = l.hostname + ((l.port.length>0)?':' + l.port:'');
+      }
+  
+      this.url = `${l.protocol}//${host}/api/auth/`;
+    }
+  
+    register(user: User): Observable<User>{
+      return this.http.post<User>(this.url + 'register', user, httpOptions);
+    }
+  
+    login(user: User): Observable<User>{
+      return this.http.post<User>(this.url + 'login', user, httpOptions);
+    }
+  
+    logout(): Observable<User>{
+      return this.http.delete<User>(this.url + 'logout');
+    }
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+// 1. Import the service
+import { AuthService } from '../auth.service';
+
+// 1. Import the user model
+import { User } from '../user.model';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +14,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+      // 2. Instantiate a new user
+      user:User = new User();
+      // 3. Instantiate an errors array
+      error: any;
+  
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}  
+    
+    // 3. Add a login method
+    onSubmit(): void{
+  
+      // this.user.username = 'testuser5';
+      // this.user.password = 'test123';
+  
+      // 2. Remove the hardcoded credentials
+      this.authService.login(this.user).subscribe(
+        (response:any)=>{
+          console.log(response);
+  
+          if(response.success == false){
+            this.error=true;
+          }
+
+          if(response.success===true){
+            // window.location.href='/#/users';
+            this.router.navigate(['/users']);          }
+        }
+      );
+    }
+
 
 }
